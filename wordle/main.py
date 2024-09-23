bfa6c193c16304b8b398272c05e2d542e557b38e
Jeremy Brensinger <jbrensinger03@gmail.com>
2024-09-23 11:43:40 -0400

    Added input validation

--- wordle/main.py
+++ wordle/main.py
@@ -2,19 +2,23 @@ from functions import *
 import random
 import os
 
-guessed = False
+words = []
+get_words(words)
 playing = True
 os.system("color")
 
-words = []
-get_words(words)
-
+# Game loop
 while playing:
     guesses = 0
+    guessed = False
     word = words[random.randint(0, len(words))]
 
+    # Game conditions
     while guesses < 6 and guessed == False:
         guessed_word = input("")
+        # Input validation
+        while len(guessed_word) != 5:
+            guessed_word = input("Enter valid word (length of 5): ")
 
         color_map = get_map(word, guessed_word.upper())
         print_color_map(guessed_word.upper(), color_map)
