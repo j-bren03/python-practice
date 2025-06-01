71ed19f035342065766d7d1c9e9ea19bec40d265
Jeremy Brensinger <jbrensinger03@gmail.com>
2025-06-01 04:45:52 -0400

    Redo alphabet mapping

--- monoalph-sub-encode-decode/main.py
+++ monoalph-sub-encode-decode/main.py
@@ -1,9 +1,7 @@
 from functions import *
 
 # Alphabet key, index is the letter value
-key = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-       'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-       'w', 'x', 'y', 'z']
+key = [chr(char) for char in range(97, 123)]
 
 msg = input("").split()
 msg = "".join(msg)
